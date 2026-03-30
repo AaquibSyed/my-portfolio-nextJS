4128aa52f2f8f0dc1b4ae51086e7376ab373fb89
Aaquib Syed <syed-aaquib.mn@capgemini.com>
2026-03-30 22:47:09 +0530

    adds layout footer navbar

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="max=w=6xl mx-auto p-6 text-center">
+        © {new Date().getFullYear()} Aaquib Syed
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
