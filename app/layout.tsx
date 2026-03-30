bf2d56c4c363cbe2b1a3af99320872085be756f2
Aaquib Syed <syed-aaquib.mn@capgemini.com>
2026-03-31 00:27:06 +0530

    adds sections and scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        <main className="flex-1 max-w-6xl mx-auto p-6">{children}</main>
+        <main className="flex-1 max-w-6xl mx-auto p-6 pt-30">{children}</main>
         <Footer />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Aaquib Syed Portfolio</h1>
+    <main>
+      <section id="about" className="min-h-screen p-10 scroll-mt-16">
+        <h1 className="text-4xl font-bold">About Me</h1>
+      </section>
 
-      <p className="mt-4">
-        Senior Software Engineer | React | Next.js | Node.js
-      </p>
-    </div>
+      <section id="skills" className="min-h-screen p-10 scroll-mt-16">
+        <h1 className="text-4xl font-bold">Skills</h1>
+      </section>
+
+      <section id="projects" className="min-h-screen p-10 scroll-mt-16">
+        <h1 className="text-4xl font-bold">Projects</h1>
+      </section>
+
+      <section id="contact" className="min-h-screen p-10 scroll-mt-16">
+        <h1 className="text-4xl font-bold">Contact</h1>
+      </section>
+    </main>
   );
 }
